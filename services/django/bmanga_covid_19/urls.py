870c3107bd75b472468bfc8f5ced5dcbec07208b
Andres Quintero Galvan <aquintero446@gmail.com>
2020-03-25 07:57:12 -0500

    fix: url routing

--- services/django/bmanga_covid_19/urls.py
+++ services/django/bmanga_covid_19/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('security_conv.urls')),
-    path('/', include('supplying.urls')),
-    path('/', include('traffic.urls')),
-    path('/', include('water.urls')),
     path('', include('frontend.urls')),
+    path('', include('security_conv.urls')),
+    path('', include('supplying.urls')),
+    path('', include('traffic.urls')),
+    path('', include('water.urls')),
 ]
